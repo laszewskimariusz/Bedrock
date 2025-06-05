bc6e09e15053d3d382c21bf18989dcdb095dc451
Mariusz Laszewski <laszewskimariusz@gmail.com>
2025-06-05 20:26:49 +0100

    fix: import ThemeToggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import ThemeToggle from '../components/theme-toggle';
 
 
 const inter = Inter({ subsets: ['latin'] });
